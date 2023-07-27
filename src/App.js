ae6d37cdf2768f42cc5b74d6c73742d34b06f5af
omnikorn <harith@Hariths-MacBook-Air-2.local>
2023-07-27 16:13:35 +0100

    setting out basing strucutres

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Appointments from './components/Appointments';
 
 function App() {
+
+  // create a state for contacts and updateing contact
+  // create a state for appointms and updateing appointment
+  // create a function to add a contact
+  //create a function to add an app
+  // create a func to delete an appointment 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Appointments />
     </div>
   );
 }
---
+++ src/components/ContCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContCard = () => {
+  return (
+    <div>ContCard</div>
+  )
+}
+
+export default ContCard
